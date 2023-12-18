c352dd694ee56cbf3354632ebe5d19651edc4af8
nicopx4009 <nicopx4009vq@gmail.com>
2023-12-18 17:40:21 -0500

    Parte Back funcionando

--- Back/index.js
+++ Back/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const sequelize = require("../Back/Model/Conexion");
-
+require("dotenv/config.js");
 // CONEXION MIDDLEWARE
 app.use(cors());
 app.use(express.json());
@@ -14,7 +14,13 @@ app.use(express.urlencoded({ extended: true }));
 const Usuario = require("../Back/Model/Usuario");
 const Calendario = require("../Back/Model/Calendario");
 const Eventos = require("../Back/Model/Eventos");
-
+//ASOCIADOS CON EL MODELO
+Usuario.sync({ logging: false }).then(async () => {
+  Usuario.sync({ logging: false });
+});
+Calendario.sync({ logging: false }).then(() => {
+  Eventos.sync({ logging: false });
+});
 
 //GENERO LAS RUTAS PARA EL MODELO MVC
 const rutaCalendario = require("../Back/routes/calendario");
@@ -24,7 +30,6 @@ const rutaEventos = require("../Back/routes/eventos");
 app.use("/calendario", rutaCalendario);
 app.use("/usuario", rutaUsuario);
 app.use("/eventos", rutaEventos);
-
 
 const conexion = async () => {
   try {
